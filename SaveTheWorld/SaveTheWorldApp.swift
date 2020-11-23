8f17e9b4b89556fad8b633a0c5281cac7a868604
Raymond Aina <rta2@hw.ac.uk>
2020-11-23 13:36:37 +0000

    Initial Commit

---
+++ SaveTheWorld/SaveTheWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveTheWorldApp.swift
+//  SaveTheWorld
+//
+//  Created by Raymond Aina on 23/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SaveTheWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
